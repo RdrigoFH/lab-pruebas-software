b3172b0b8b898bf59751120b859821f68ea9ea4d
RodrygoLeu <rodrygoleu7@gmail.com>
2026-05-18 15:23:18 -0500

    feat: adding movimiento class and validating it with the test cases

--- LAB_04/code/gestor_inventario/src/main/java/com/gestorinventario/Movimiento.java
+++ LAB_04/code/gestor_inventario/src/main/java/com/gestorinventario/Movimiento.java
@@ -8,9 +8,19 @@ public class Movimiento {
   private int cantidad;
   private LocalDateTime fecha;
 
-  public Movimiento(TipoMovimiento tipo, int cantidad) {}
+  public Movimiento(TipoMovimiento tipo, int cantidad) {
+    this(tipo, cantidad, LocalDateTime.now());
+  }
 
-  public Movimiento(TipoMovimiento tipo, int cantidad, LocalDateTime fecha) {}
+  public Movimiento(TipoMovimiento tipo, int cantidad, LocalDateTime fecha) {
+    validarTipo(tipo);
+    validarCantidad(cantidad);
+    validarFecha(fecha);
+
+    this.tipo = tipo;
+    this.cantidad = cantidad;
+    this.fecha = fecha;
+  }
 
   public TipoMovimiento getTipo() {
     return tipo;
@@ -22,5 +32,27 @@ public class Movimiento {
 
   public LocalDateTime getFecha() {
     return fecha;
+  }
+
+  private static void validarTipo(TipoMovimiento tipo) {
+    if (tipo == null) {
+      throw new IllegalArgumentException("El tipo no puede ser nulo");
+    }
+  }
+
+  private static void validarCantidad(int cantidad) {
+    if (cantidad <= 0) {
+      throw new IllegalArgumentException("La cantidad debe ser mayor que cero");
+    }
+  }
+
+  private static void validarFecha(LocalDateTime fecha) {
+    if (fecha == null) {
+      throw new IllegalArgumentException("La fecha no puede ser nula");
+    }
+
+    if (fecha.isAfter(LocalDateTime.now())) {
+      throw new IllegalArgumentException("La fecha no puede ser futura");
+    }
   }
 }
