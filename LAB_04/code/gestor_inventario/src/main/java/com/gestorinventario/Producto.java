d5a23f41e89f370fa2d7edfb7dc26960cbcea28d
RodrygoLeu <rodrygoleu7@gmail.com>
2026-05-17 19:30:46 -0500

    feat: adding products methods

--- LAB_04/code/gestor_inventario/src/main/java/com/gestorinventario/Producto.java
+++ LAB_04/code/gestor_inventario/src/main/java/com/gestorinventario/Producto.java
@@ -5,4 +5,20 @@ public class Producto {
     public String nombre;
     public double precio;
     public int cantidad;
+
+    public void agregarStock(int cantidad) {
+        this.cantidad += cantidad;
+    }
+
+    public void extraerStock(int cantidad) {
+        this.cantidad -= cantidad;
+    }
+
+    public int consultarStock() {
+        return cantidad;
+    }
+
+    public double obtenerValorTotal() {
+        return precio * cantidad;
+    }
 }
