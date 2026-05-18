c9664a1f805c1608836038217eb1060de0bd1010
dmolloc1 <dmolloc@unsa.edu.pe>
2026-05-17 19:09:20 -0500

    feat(carrito): crear clases ItemCarrito y CarritoCompra con atributos y constructores

---
+++ LAB_04/sources/lab_04/src/main/java/com/lab04/Ejercicio2/ItemCarrito.java
@@ -0,0 +1,10 @@
+package main.java.com.lab04.Ejercicio2;
+public class ItemCarrito {
+    private Producto producto;
+    private int cantidad;
+
+    public ItemCarrito(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+}
