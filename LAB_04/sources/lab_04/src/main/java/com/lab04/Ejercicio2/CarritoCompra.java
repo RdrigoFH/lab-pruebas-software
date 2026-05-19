6fe476af7d0f39b11e965b9c425e6466953fca83
dmolloc1 <dmolloc@unsa.edu.pe>
2026-05-19 11:41:59 -0500

    feat(carrito): agregar método para actualizar cantidad

--- LAB_04/sources/lab_04/src/main/java/com/lab04/Ejercicio2/CarritoCompra.java
+++ LAB_04/sources/lab_04/src/main/java/com/lab04/Ejercicio2/CarritoCompra.java
@@ -1,5 +1,8 @@
 package main.java.com.lab04.Ejercicio2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarritoCompra {
     private List<ItemCarrito> items;
     private ServicioPrecio servicioPrecio;
@@ -11,10 +14,38 @@ public class CarritoCompra {
         this.total = 0.0;
     }
     public void agregarProducto(Producto producto, int cantidad) {
+        try {
+            cantidadValida(cantidad);
+            productoDisponible(producto);
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            System.out.println("Error al agregar producto: " + e.getMessage());
+            return;
+        }
+
         ItemCarrito item = new ItemCarrito(producto, cantidad);
         this.items.add(item);
     }
 
+    private void cantidadValida(int cantidad) {
+        if (cantidad <= 0) {
+                throw new IllegalArgumentException("La cantidad tiene que ser mayor a cero");
+        }
+    }
+    private void productoDisponible(Producto producto) {
+        if (!producto.getDisponibilidad()) {
+            throw new IllegalStateException("El producto no esta disponible");
+        }
+    }
+
+    public void actualizarCantidad(Producto producto, int nuevaCantidad) {
+        cantidadValida(nuevaCantidad);
+        for (ItemCarrito item : items) {
+            if (item.getProducto().getId() == producto.getId()) {
+                item.setCantidad(nuevaCantidad);
+                break;
+            }
+        }
+    }
     public void eliminarProducto(Producto producto) {
         for (ItemCarrito item : items) {
             if (item.getProducto().getId() == producto.getId()) {
@@ -43,7 +74,10 @@ public class CarritoCompra {
         return precioFinal + impuesto;
     }
     public void obtenerResumen(){
-        
+        System.out.println("Resumen de productos:");
+        for (ItemCarrito item : items) {
+            System.out.println("Producto: " + item.getProducto().getNombre() + ", Cantidad: " + item.getCantidad() + ", Precio: " + item.getProducto().getPrecio());
+        }
     }
 
 }
